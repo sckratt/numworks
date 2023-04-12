612ec5a9e859d6b423137f577528e593a1eeaf3e
Sckratt <67607934+sckratt@users.noreply.github.com>
2023-04-12 17:18:41 +0200

    Update congruences.py

--- congruences.py
+++ congruences.py
@@ -1,9 +1,9 @@
 import sys
 sys.set_int_max_str_digits(8000)
 
-def congruences():
+def reste_pow():
     print("-------------------------------")
-    print("CONGRUENCES - PUISSANCE")
+    print("TROUVER LE RESTE - PUISSANCE")
     print("-------------------------------")
     dividende = input("DIVIDENDE: ")
     nbr = int(dividende.split("**")[0])
@@ -40,3 +40,28 @@ def congruences():
     print(f"{dividende} ≡ {congr}**({n}x{pdiv}+{prest})")
     print(f"≡ ({congr}**{n})**{pdiv} x {congr}**{prest}")
     print(f"≡ {congr}**{prest} ≡ {congr**prest} ≡ {(congr**prest)%modulo} [{modulo}]")
+
+def reste_pow_n():
+    print("-------------------------------")
+    print("TROUVER RESTE AVEC N - PUISSANCE")
+    print("-------------------------------")
+    dividende = input("DIVIDENDE: ")
+    modulo = int(input("MODULO: "))
+    nbr = int(dividende.split("**")[0])
+    p = dividende.split("**")[1]
+
+    print(f"On recherche le plus petit n ∈ ℕ tel que {nbr}**n ≡ 1 [{modulo}]")
+    n = 2
+    while (nbr**n)%modulo != 1:
+        n += 1
+    print(f"On a donc n = {n}")
+    print(f"On fait alors un tableau des reste de n=0 à n={n-1}")
+    # On fait le tableau de 0 à n-1
+    tab = []
+    for i in range(n):
+        nb = str((nbr**i)%modulo)
+        if not nb in tab:
+            tab.append(nb)
+    print("On a alors les reste suivants: ")
+    print(f"{p}  {' ' * len(p)}:", " ".join(list(map(lambda i: "{:<3}".format(i), [i for i in range(n)]))))
+    print(f"{nbr}**{p}:", " ".join(list(map(lambda r: "{:<3}".format(r), tab))))
